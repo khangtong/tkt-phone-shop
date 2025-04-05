06da789646e13598b5b20e5b78004984d4d87c3f
Khangtong <duykhangtong99@gmail.com>
2025-04-05 17:19:39 +0700

    update statistic

--- backend/controllers/statisticController.js
+++ backend/controllers/statisticController.js
@@ -1,74 +1,110 @@
 import Order from '../models/orderModel.js';
 import OrderDetail from '../models/orderDetailModel.js';
+import Category from '../models/categoryModel.js';
 
 export const getStatistics = async (req, res) => {
   try {
-    const { start, end } = req.body;
+    const { start, end } = req.query;
 
-    const orders = await Order.find({
-      orderDate: {
-        $gte: new Date(start),
-        $lte: new Date(end),
-      },
-    });
-    console.log(orders);
+    let orders;
+
+    if (new Date(start).getTime() == new Date(end).getTime()) {
+      orders = await Order.find({
+        orderDate: {
+          $gte: new Date(start),
+        },
+        status: { $eq: 'Success' },
+      });
+    } else if (new Date(start).getTime() > new Date(end).getTime()) {
+      return res.status(400).json({ message: 'Invalid date range' });
+    } else {
+      orders = await Order.find({
+        $and: [
+          {
+            orderDate: {
+              $gte: new Date(start),
+            },
+          },
+          {
+            orderDate: {
+              $lte: new Date(end),
+            },
+          },
+        ],
+        status: { $eq: 'Success' },
+      });
+    }
 
     if (!orders || orders.length === 0) {
       return res.status(404).json({ message: 'No orders found' });
     }
 
     const pipeline = [
-      { $in: orders.map((order) => order._id) },
+      {
+        $match: {
+          orderId: { $in: orders.map((o) => o._id) },
+        },
+      },
+      {
+        $lookup: {
+          from: 'orders',
+          localField: 'orderId',
+          foreignField: '_id',
+          as: 'order',
+        },
+      },
       {
         $lookup: {
           from: 'products',
           localField: 'productId',
           foreignField: '_id',
+          pipeline: [
+            {
+              $lookup: {
+                from: 'categories',
+                localField: 'category',
+                foreignField: '_id',
+                as: 'category',
+              },
+            },
+          ],
           as: 'product',
         },
       },
       {
         $lookup: {
-          from: 'carts',
-          localField: 'cartId',
+          from: 'variations',
+          localField: 'variationId',
           foreignField: '_id',
-          pipeline: [
-            {
-              $lookup: {
-                from: 'cartDetails',
-                localField: 'cartDetails',
-                foreignField: '_id',
-                pipeline: [
-                  {
-                    $lookup: {
-                      from: 'variations',
-                      localField: 'variation',
-                      foreignField: '_id',
-                      pipeline: [
-                        {
-                          $lookup: {
-                            from: 'discounts',
-                            localField: 'discount',
-                            foreignField: '_id',
-                            as: 'discount',
-                          },
-                        },
-                      ],
-                      as: 'variation',
-                    },
-                  },
-                ],
-                as: 'cartDetail',
-              },
-            },
-          ],
-          as: 'cart',
+          as: 'variation',
         },
       },
     ];
 
     const orderDetails = await OrderDetail.aggregate(pipeline);
-    res.status(200).json(orderDetails);
+    const categories = await Category.find({});
+
+    const statistic = {
+      totalRevenue: orders.reduce((acc, order) => acc + order.total, 0),
+      totalOrders: orders.length,
+    };
+
+    statistic.revenueOnCategory = categories.map((category) => {
+      const categoryOrders = orderDetails.filter(
+        (detail) =>
+          detail.product[0].category[0]._id.toString() ===
+          category._id.toString()
+      );
+      return {
+        category: category.name,
+        totalRevenue: categoryOrders.reduce(
+          (acc, order) => acc + order.priceAtPurchase * order.quantity,
+          0
+        ),
+      };
+    });
+
+    res.status(200).json(statistic);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
